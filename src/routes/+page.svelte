d6428487b51707c8f0ea40c7f8d9796d23549ca1
muckelba <34460584+muckelba@users.noreply.github.com>
2025-01-17 00:50:29 +0100

    fix background image and home icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -578,7 +578,15 @@
     <div class="level-left">
       <div class="level-item" style="justify-content: normal">
         <a class="icon is-medium" href="/" rel="external" title="Home">
-          <i class="fa fa-home fa-lg" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" color="currentColor" role="img" aria-label="home outline" viewBox="0 0 24 24">
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"
+            />
+          </svg>
         </a>
       </div>
     </div>
